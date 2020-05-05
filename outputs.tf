fb90123834df6110204f31369c3e045c5f586a95
Peter Salnikov <peter@salnikov.cc>
2020-05-05 15:26:54 +0300

    add repo url

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "lifecycle_policy" {
 output "ecr_iam_policy" {
   value = data.aws_iam_policy_document.this.json
 }
+
+output "repository_url" {
+  value = aws_ecr_repository.this.repository_url
+}
